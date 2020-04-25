1424c8ad3f6b50937ec1b96eb45cab2e819bd205
bopboy2 <scp0000@gmail.com>
2020-04-25 21:19:41 +0900

    02 using url parameter and query

--- src/About.js
+++ src/About.js
@@ -1,10 +1,15 @@
 import React from "react";
+import qs from "qs";
 
-function About() {
+function About({ location }) {
+    const query = qs.parse(location.search, { ignoreQueryPrefix: true });
+    const detail = query.detail === 'true';
+
     return (
         <div>
             <h1>About</h1>
             <p>This is About Page</p>
+            {detail && <p>detail 값이 true 로 설정되었습니다</p>}
         </div>);
 }
 
---
+++ src/Profile.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const profileData = {
+    lee: { name: 'lee sung', description: 'front engineer @ bopboy' },
+    homer: { name: 'homer simpson', description: 'father of the simpsons' }
+};
+function Profile({ match }) {
+    const { username } = match.params;
+    const profile = profileData[username];
+
+    if (!profile) {
+        return <div>존재하지 않는 사용자입니다</div>
+    }
+
+    return (
+        <div>
+            <h3>{username} ({profile.name})</h3>
+            <p>{profile.description}</p>
+        </div>
+    );
+}
+
+export default Profile;
