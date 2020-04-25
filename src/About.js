f59356493b240abcbc824d8a347d34b6a971baa6
bopboy2 <scp0000@gmail.com>
2020-04-25 19:55:23 +0900

    01 기본 사용

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is About Page</p>
+        </div>);
+}
+
+export default About;
